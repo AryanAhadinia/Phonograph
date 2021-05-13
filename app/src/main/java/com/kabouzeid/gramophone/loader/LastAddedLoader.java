79069adf1dd5a2b2800cf4077b25f55fd500e491
Aryan Ahadinia <62179268+AryanAhadinia@users.noreply.github.com>
2021-05-13 21:47:33 +0430

    Add limit for last added songs

--- app/src/main/java/com/kabouzeid/gramophone/loader/LastAddedLoader.java
+++ app/src/main/java/com/kabouzeid/gramophone/loader/LastAddedLoader.java
@@ -3,19 +3,33 @@ package com.kabouzeid.gramophone.loader;
 import android.content.Context;
 import android.database.Cursor;
 import android.provider.MediaStore;
-import androidx.annotation.NonNull;
 
+import androidx.annotation.NonNull;
+import androidx.preference.PreferenceManager;
+
+import com.kabouzeid.gramophone.R;
 import com.kabouzeid.gramophone.model.Song;
 import com.kabouzeid.gramophone.util.PreferenceUtil;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class LastAddedLoader {
 
     @NonNull
     public static List<Song> getLastAddedSongs(@NonNull Context context) {
-        return SongLoader.getSongs(makeLastAddedCursor(context));
+        int defaultLimit = context.getResources().getInteger(R.integer.default_last_added_limit);
+        int limit = toIntOrDefault(PreferenceManager.getDefaultSharedPreferences(context)
+                .getString("last_added_limit", String.valueOf(defaultLimit)), defaultLimit);
+        List<Song> songs = SongLoader.getSongs(makeLastAddedCursor(context));
+        return songs.subList(0, Math.min(songs.size(), limit));
+    }
+
+    public static int toIntOrDefault(String s, int d) {
+        try {
+            return Integer.parseInt(s);
+        } catch (NumberFormatException e) {
+            return d;
+        }
     }
 
     public static Cursor makeLastAddedCursor(@NonNull final Context context) {
